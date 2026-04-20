3b2b87292d762e971e0c8b39021ee716106a2de1
Suraj Rajput <surajpatil108>
2026-04-20 22:31:32 +0530

    Added select domains

--- my_profile/urls.py
+++ my_profile/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('domains/', views.domains, name='domains'),
 ]
--- my_profile/views.py
+++ my_profile/views.py
@@ -6,3 +6,11 @@ def home(request):
     projects = Project.objects.all().order_by('-created_at')
     # Send them to the 'index.html' template
     return render(request, 'index.html', {'projects': projects})
+
+def domains(request):
+    domains = [
+        {'name': 'Web Development', 'description': 'Building responsive and dynamic websites using Django, React, and more.'},
+        {'name': 'services', 'description': 'Offering a range of services including web development, consulting, and training.'},
+        {'name': 'Blog', 'description': 'Content & news platforms'},
+    ]
+    return render(request, 'domains.html', {'domains': domains})
